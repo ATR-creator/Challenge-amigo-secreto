4352ab8c8546519b0cdc0afa704372b558a752d5
ATR <8888auron8888@gmail.com>
2025-08-12 18:37:26 -0500

    Actualiza el app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 let nombres =[];
 let nombre = "";
 
+function condiciones_iniciales() {
+    nombres =[];
+    nombre = "";
+    document.querySelector("#amigo").value = "";
+}
+
 function asignar_texto_elemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
@@ -9,14 +15,28 @@ function asignar_texto_elemento(elemento, texto) {
 function agregar_amigo() {
     nombre = document.querySelector("#amigo").value;
     if (nombre == "") {
-        alert("Inserte un valor válido...")
+        alert("Inserte un valor válido...");
     } else {
         nombres.push(nombre);
         limpiar_caja();
         asignar_texto_elemento("ul",nombres.join("<br>"));
-    }
-}
+    };
+};
 
 function limpiar_caja() {
     document.querySelector("#amigo").value = "";
 };
+
+function sortear_amigo() {
+    document.querySelector("#amigo").value = "";
+    if (nombres.length == 0) {
+        alert("Inserte al menos un valor");
+    } else {
+        let sorteado = Math.floor(Math.random()*nombres.length);
+        asignar_texto_elemento("ul","")
+        asignar_texto_elemento("#resultado",`Tu amigo secreto es: ${nombres[sorteado]}`);
+        condiciones_iniciales();
+    };
+};
+
+condiciones_iniciales();
